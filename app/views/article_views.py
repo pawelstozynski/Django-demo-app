47cfa798451a39ca350aea5a438e93b2c0b1081e
Pablo <pawelstozynski@gmail.com>
2018-06-20 23:59:05 +0200

    HTTP method view decorators

--- app/views/article_views.py
+++ app/views/article_views.py
@@ -1,28 +1,30 @@
+from django.views.decorators.http import require_GET, require_POST, require_http_methods
 from django.shortcuts import render, redirect, HttpResponse, get_object_or_404
 
 from ..forms import *
 from ..models import *
 
 
+@require_GET
 def index(request):
     articles = Article.objects.all()
     return render(request, 'article/index.html', {'articles':articles})
 
 
+@require_POST
 def article_search(request):
-    if request.method == 'POST':
-        search_string = request.POST['search']
-        articles = Article.objects.filter(name__contains=search_string)
-        return render(request, 'article/index.html', {'articles':articles})
-    else:
-        return HttpResponse('Not allowed')
+    search_string = request.POST['search']
+    articles = Article.objects.filter(name__contains=search_string)
+    return render(request, 'article/index.html', {'articles':articles})
 
 
+@require_GET
 def article_show(request, article_id):
     article = get_object_or_404(Article, id=article_id)
     return render(request, 'article/show.html', {'article':article})
 
 
+@require_http_methods(['GET', 'POST'])
 def article_create(request):
     if request.method == 'POST':
         form = ArticleForm(request.POST)
@@ -34,6 +36,7 @@ def article_create(request):
         return render(request, 'article/create.html', {'form':form})
 
 
+@require_http_methods(['GET', 'POST'])
 def article_edit(request, article_id):
     article = get_object_or_404(Article, id=article_id)
     if request.method == 'POST':
@@ -46,10 +49,8 @@ def article_edit(request, article_id):
         return render(request, 'article/edit.html', {'form':form, 'article':article})
 
 
+@require_POST
 def article_delete(request, article_id):
-    if request.method == 'POST':
-        article = get_object_or_404(Article, id=article_id)
-        article.delete()
-        return redirect('index')
-    else:
-        return HttpResponse('Not allowed')
+    article = get_object_or_404(Article, id=article_id)
+    article.delete()
+    return redirect('index')
